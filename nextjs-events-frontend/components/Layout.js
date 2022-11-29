055365e5360eba0352496f2df35963f05592937f
kassiobruno <kaassio1@gmail.com>
2022-11-29 10:53:07 -0300

    Layout ajustes

--- nextjs-events-frontend/components/Layout.js
+++ nextjs-events-frontend/components/Layout.js
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import styles from "../styles/Layout.module.css"
 
 export default function Layout({ title, keywords, description, children}) {
   return (
@@ -9,7 +10,16 @@ export default function Layout({ title, keywords, description, children}) {
         <meta name="keywords" content={keywords}/>
       </Head>
 
+      <div className={styles.container}>
       {children}
+      </div>
+
     </div>
   )
+}
+
+Layout.defaultProps = {
+  title: 'DJ Events | Encontre as melhores festas musicais',
+  description: 'Encotre os melhores DJ e outros eventos musicais',
+  keywords: 'musica, dj, eventos'
 }
--- nextjs-events-frontend/pages/about.js
+++ nextjs-events-frontend/pages/about.js
@@ -1,12 +1,13 @@
 import Link from "next/link"
+import Layout from "../components/Layout"
 
 export default function AboutPage() {
   return (
-    <div>
+    <Layout title='About DJ Events'>
       <h1>About</h1>
       <p>Esse é um App para encontrar eventos de DJs e outros eventos musicais</p>
       <p>Versao: 1.0.0</p>
       <Link href="/">Home</Link>
-    </div>
+    </Layout>
   )
 }
