7f40c8274f136b464f7f7dd9f59bfafe7436616b
kassiobruno <kaassio1@gmail.com>
2022-11-30 08:52:43 -0300

    Layout, Header and Footer

---
+++ nextjs-events-frontend/components/Header.js
@@ -0,0 +1,25 @@
+import Link from "next/link"
+import styles from "../styles/Header.module.css"
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link href='/' legacyBehavior>
+          <a>DJ EVENTS</a>
+        </Link>
+      </div>
+
+      <nav>
+        <ul>
+          <li>
+            <Link href='/events' legacyBehavior>
+              <a>Eventos</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+    </header>
+  )
+}
